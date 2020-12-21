c78779ebe862ebe823c4d85af4b81415820134f3
songxiaoyu <823945570@qq.com>
2020-12-21 10:56:12 +0800

    测试提交邮箱

---
+++ app/src/main/java/com/sxy/jetpackdemo/app/weight/FatScrollAwareFABBehavior.kt
@@ -0,0 +1,65 @@
+package com.sxy.jetpackdemo.app.weight
+
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+/**
+ * @author: sxy
+ * @date: 2020/12/21
+ * @description: 自定义behavior fab上划显示 下滑隐藏
+ */
+class FatScrollAwareFABBehavior : FloatingActionButton.Behavior(){
+
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(
+            coordinatorLayout,
+            child,
+            directTargetChild,
+            target,
+            axes,
+            type
+        )
+    }
+
+    override fun onNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        target: View,
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        type: Int,
+        consumed: IntArray
+    ) {
+        super.onNestedScroll(
+            coordinatorLayout,
+            child,
+            target,
+            dxConsumed,
+            dyConsumed,
+            dxUnconsumed,
+            dyUnconsumed,
+            type,
+            consumed
+        )
+
+
+        if(dyConsumed > 0 && child.visibility != View.VISIBLE){
+            child.visibility  = View.INVISIBLE
+        }else if (dyConsumed < 0 && child.visibility != View.VISIBLE){
+            child.show()
+        }
+    }
+
+}
