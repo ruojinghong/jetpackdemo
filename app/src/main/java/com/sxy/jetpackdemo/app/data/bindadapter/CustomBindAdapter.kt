078b0969d44f504ef3b2a3f541661da22b0357ee
songxiaoyu <823945570@qq.com>
2020-12-30 23:43:14 +0800

    自定义bindadapter

---
+++ app/src/main/java/com/sxy/jetpackdemo/app/data/bindadapter/CustomBindAdapter.kt
@@ -0,0 +1,24 @@
+package com.sxy.jetpackdemo.app.data.bindadapter
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+
+/**
+ * 自定义bindadapter
+ */
+object CustomBindAdapter {
+
+    @BindingAdapter(value = ["circleImageUrl"])
+    @JvmStatic
+    fun circleImageUrl(view: ImageView, url: String) {
+        Glide.with(view.context.applicationContext)
+            .load(url)
+            .apply(RequestOptions.bitmapTransform(CircleCrop()))
+            .transition(DrawableTransitionOptions.withCrossFade(500))
+            .into(view)
+    }
+}
